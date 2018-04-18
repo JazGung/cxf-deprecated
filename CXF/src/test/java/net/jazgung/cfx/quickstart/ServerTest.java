becb7ad2584368199374bcbcedf15a9abfc7bc5c
JazGung <jazgung@gmail.com>
2018-04-18 15:05:52 +0800

    修改错误

--- CXF/src/test/java/net/jazgung/cfx/quickstart/ServerTest.java
+++ CXF/src/test/java/net/jazgung/cfx/quickstart/ServerTest.java
@@ -1,5 +1,7 @@
 package net.jazgung.cfx.quickstart;
 
+import org.junit.Test;
+
 import net.jazgung.cfx.GenericTest;
 import net.jazgung.cfx.quickstart.dto.ReqDto;
 import net.jazgung.cfx.quickstart.server.Server;
@@ -10,15 +12,13 @@ import net.jazgung.cfx.quickstart.webservice.QuickStartServiceImpl;
 import net.jazgung.cfx.util.ClientFactory;
 import net.jazgung.cfx.webservice.WebServiceUtils;
 
-import org.junit.Test;
-
 public class ServerTest extends GenericTest {
 	private void invoke(Server server) {
 		server.startup(QuickStartService.class, new QuickStartServiceImpl());
 
-		QuickStartService service = (new ClientFactory<QuickStartService>(WebServiceUtils.calcWsdlhUrl(QuickStartService.class,
-				QuickStartService.ADDRESS), QuickStartService.SERVICE_NAME)).getService(QuickStartService.PORT_NAME,
-				QuickStartService.class);
+		QuickStartService service = (new ClientFactory<QuickStartService>(
+				WebServiceUtils.calcWsdlhUrl(QuickStartService.class, QuickStartService.ADDRESS), QuickStartService.SERVICE_NAME))
+						.getService(QuickStartService.PORT_NAME, QuickStartService.class);
 		ReqDto req = new ReqDto();
 		req.setWord("Hello World");
 		System.out.println("echo: " + service.service(req).getEcho());
@@ -42,4 +42,5 @@ public class ServerTest extends GenericTest {
 		invoke(server);
 		printCutOffRule("end testJaxWsServerFactoryBean");
 	}
+
 }
